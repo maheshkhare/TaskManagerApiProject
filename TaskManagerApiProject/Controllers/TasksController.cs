e6cff69cf77c99054db5e0ea7c42f874a10f3fc6
Mahesh <mahesh.khare@alphamd.com>
2025-05-18 00:49:06 +0530

    added Api call for getting task

---
+++ TaskManagerApiProject/Controllers/TasksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApiProject.Data;
+using TaskManagerApiProject.Models;
+
+namespace TaskManagerApiProject.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class TasksController : ControllerBase
+        {
+            private readonly TaskContext _context;
+
+            public TasksController(TaskContext context)
+            {
+                _context = context;
+            }
+
+            // GET: api/tasks
+            [HttpGet]
+            public async Task<ActionResult<IEnumerable<TaskModel>>> GetTasks()
+            {
+                return await _context.Tasks.ToListAsync();
+            }
+
+          
+        }
+}
