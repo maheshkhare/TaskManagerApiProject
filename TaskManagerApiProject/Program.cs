3ba493955657764c666fdc5a2e9f86a9ac54143a
Mahesh <mahesh.khare@alphamd.com>
2025-05-18 01:14:56 +0530

    added dependancies for inmemory database and for the swagger

--- TaskManagerApiProject/Program.cs
+++ TaskManagerApiProject/Program.cs
@@ -1,17 +1,36 @@
+﻿using TaskManagerApiProject.Data;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApiProject.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Register EF Core In-Memory DB
+builder.Services.AddDbContext<TaskContext>(opt =>
+    opt.UseInMemoryDatabase("TaskList"));
 
+// Add services to the container
 builder.Services.AddControllers();
+
+// ✅ Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 
-app.UseHttpsRedirection();
+// ✅ Enable Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
