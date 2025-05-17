ce38193a54e4737fda38251802498008412e31f3
Mahesh <mahesh.khare@alphamd.com>
2025-05-18 00:42:03 +0530

    Created TaskContext class to handle EF Core DB operations

---
+++ TaskManagerApiProject/Data/TaskContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TaskManagerApiProject.Models;
+
+namespace TaskManagerApiProject.Data
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
