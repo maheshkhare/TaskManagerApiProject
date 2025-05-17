ca1e16ec51a52eb80bff92c254a33879db989107
Mahesh <mahesh.khare@alphamd.com>
2025-05-18 00:38:07 +0530

    Added Task model

---
+++ TaskManagerApiProject/Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManagerApiProject.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
